c3b7b2d6bcd76bf84c5858b125874371a6628a75
MelentQ <mejleht@yandex.ru>
2024-10-14 11:36:13 +0300

    Увеличение скорости загрузки сайта - яндекс карты

--- src/js/modules/renderYandexMapApi.js
+++ src/js/modules/renderYandexMapApi.js
@@ -1,13 +1,31 @@
-export default function renderYandexMapApi(params) {
-  const mapsWrappers = document.querySelectorAll('.js-footer-map');
+export default function renderYandexMapApi(containerSelector, callback) {
+  const mapsWrappers = document.querySelectorAll(containerSelector);
 
   if(mapsWrappers.length > 0) {
-    const script = document.createElement('script');
-    script.src = "https://api-maps.yandex.ru/2.1/?apikey=72c05200-f928-4e05-aed9-e60fedabd791&lang=ru_RU";
-    document.body.append(script);
-
-    script.onload = () => {
-      return true;
+    const apiKey = mapsWrappers[0].getAttribute('data-api-key');
+    if (!apiKey) {
+      console.warn(`Не указан API-ключ (data-api-key) для Яндекс карт ${containerSelector}`)
+      return;
     }
+
+    function init() {
+      const script = document.createElement('script');
+      script.src = `https://api-maps.yandex.ru/2.1/?apikey=${apiKey}&lang=ru_RU`;
+      script.onload = callback;
+      document.body.append(script);
+    }
+
+    function onScroll() {
+      if (window.scrollY > 100) {
+        init();
+        window.removeEventListener('scroll', onScroll);
+      }
+    }
+
+    window.addEventListener('scroll', onScroll, {
+      passive: true,
+    });
+
+    onScroll();
   }
 }
